e3c23577b3924984dcb1d28e4ddb58de06763b08
日光 <44632806+zhugwangzi@users.noreply.github.com>
2021-11-22 09:03:39 +0800

    更新版本

--- src/chaptor1_3/Test04.java
+++ src/chaptor1_3/Test04.java
@@ -1,35 +1,28 @@
 package chaptor1_3;
 import edu.princeton.cs.algs4.*;
 public class Test04 {
+	public static boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='('||c=='['||c=='{'){
+                stack.push(c);
+            }else{
+            	
+            	if(stack.isEmpty()||!match(stack.pop(),c)) {return false;}        		
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static boolean match(char c1,char c2){
+        if(c1=='('&&c2==')'){ return true;}
+        if(c1=='['&&c2==']'){ return true;}
+        if(c1=='{'&&c2=='}'){ return true;}
+        return false;
+    }
 	public static void main(String[] args) {
 		System.out.println("输入含有 (){}[] 的字符串:");
 		String str=StdIn.readLine();
-		String[] s=str.split("");
-		Stack<String> stack=new Stack<String>();
-		for(String x:s) {//遍历每个括号
-			if(x.equals("{")||x.equals("[")||x.equals("(")) {//遇到左括号就入栈
-				stack.push(x);
-			}else {//遇到右括号就出栈进行匹配
-				if(x.equals(")")) {//若右括号是 ')'
-					if(!stack.pop().equals("(")) {//如果出栈的元素没匹配到 '('就错误
-						System.out.println("false");
-						return;
-					}
-				}
-				if(x.equals("]")) {//若右括号是 ']'
-					if(!stack.pop().equals("[")) {//如果出栈的元素没匹配到 '['就错误
-						System.out.println("false");
-						return;
-					}
-				}
-				if(x.equals("}")) {//若右括号是 '}'
-					if(!stack.pop().equals("{")) {//如果出栈的元素没匹配到 '{就错误
-						System.out.println("false");
-						return;
-					}
-				}
-			}
-		}
-		System.out.println("true");//没有错误则输出true
+		System.out.println(isValid(str));
 	}
 }
