deb46d60fb9962bad9f38d877ffc3727d1f121a1
日光 <44632806+zhugwangzi@users.noreply.github.com>
2021-12-09 01:05:43 +0800

    Add files via upload

--- src/chaptor1_4/Test18.java
+++ src/chaptor1_4/Test18.java
@@ -1,7 +1,7 @@
 package chaptor1_4;
 
 public class Test18 {
-	static int N;//递归次数
+	static int N;//查找次数
 	public static int localMin(double[] a) {
 		N=0;
 		return find2(a, 0, a.length-1);//可以修改为find1
@@ -10,7 +10,9 @@ public class Test18 {
 		//严格按照题意，局部最小值必须同时小于左右两个数，不包含端点
 		N++;
 		int mid=low+(high-low)/2;
-		if(low>high||mid==0||mid==a.length-1) {return -1;}//找不到
+		//找不到
+		if(low>high||mid==0||mid==a.length-1) {return -1;}
+		//找到了
 		if(a[mid]<a[mid-1]&&a[mid]<a[mid+1]) {return mid;}
 		int i=find1(a, low, mid-1);
 		return i!=-1?i:find1(a, mid+1, high);
@@ -25,13 +27,25 @@ public class Test18 {
 		if(a[mid-1]<a[mid+1]){return find2(a, low, mid-1);}
 		else{return find2(a, mid+1, high);}
 	}
-
+	//循环解决
+	public static int localMin2(int[] a){
+		N=0;
+		int low=0,high=a.length-1;
+		while (low<=high){
+			N++;
+			int mid=low+(high-low)/2;
+			if(mid>0&&a[mid]>a[mid-1]){high=mid-1;}
+			else if(mid<a.length-1&&a[mid]>a[mid+1]){low=mid+1;}
+			else {return mid;}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
-		double[] a1= {6,4};
-		double[] a2= {1,3,2,4,0,5,6,8,7,9,-1};
-		double[] a3= {1,2,3,4,5,6,7,8,9,10};
-		System.out.println("索引："+localMin(a1)+"，递归次数："+N);
-		System.out.println("索引："+localMin(a2)+"，递归次数："+N);
-		System.out.println("索引："+localMin(a3)+"，递归次数："+N);
+		int[] a1= {6,4};
+		int[] a2= {1,3,2,4,0,5,6,8,7,9,-1};
+		int[] a3= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("索引："+localMin2(a1)+"，查找次数："+N);
+		System.out.println("索引："+localMin2(a2)+"，查找次数："+N);
+		System.out.println("索引："+localMin2(a3)+"，查找次数："+N);
 	}
 }
